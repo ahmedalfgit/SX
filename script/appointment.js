5231e7c40a13ddee5a9665c61648fb3a5a251b25
ahmedalfgit <ahmedalf75@gmail.com>
2019-11-04 16:13:35 -0700

    new fixing

---
+++ script/appointment.js
@@ -0,0 +1,58 @@
+
+// Set the date for the input of the new job form
+let today = new Date().toISOString().substr(0, 10);
+document.querySelector("#appointmentDate").value = today;
+
+function f(id) {
+    return document.getElementById(id).value;
+}
+
+function showForm() {
+    document.getElementById("newAppointmentFormDiv").style.display = "block";
+}
+
+function submitForm () {
+    return  inputData = {
+        meetingatitle: f("appointmentTitle"),
+        meetingDate: f("appointmentDate"),
+        startTime: f("startTime"),
+        startTimeMeridiem: f("startTimeMeridiem"),
+        endTime: f("endTime"),
+        endTimeMeridiem: f("endTimeMeridiem"),
+        room: f("room")
+    }
+}
+
+function storeData() {
+    window.localStorage.setItem('inputData', JSON.stringify(submitForm()));
+    loadGoogleTable();
+}
+
+function loadGoogleTable() {
+
+    google.charts.load('current', {'packages':['table']});
+    google.charts.setOnLoadCallback(drawTable);
+
+    function drawTable() {
+        var data = new google.visualization.DataTable();
+        var tableData = JSON.parse( localStorage.getItem("inputData"));
+        console.log(tableData);
+        data.addColumn('string', 'Title');
+        data.addColumn('string', 'Date');
+        data.addColumn('string', 'Starts at');
+        data.addColumn('string', 'Ends at');
+        data.addColumn('string', 'Location');
+        data.addRows([
+            [tableData.meetingatitle,  tableData.meetingDate, tableData.startTime + " " + tableData.startTimeMeridiem, tableData.endTime + " " + tableData.endTimeMeridiem, tableData.room],
+        ]);
+
+        var table = new google.visualization.Table(document.getElementById('table_div'));
+
+        table.draw(data, {width: '100%', height: '100%'});
+    }
+    hideForm()
+}
+
+function hideForm() {
+    document.getElementById("newAppointmentFormDiv").style.display = "none";
+}
